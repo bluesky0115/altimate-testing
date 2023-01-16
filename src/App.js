49a86eefdacef861d32a5b95c5cdfce209c546a7
holinight <asmakech115@gmail.com>
2023-01-17 01:21:15 +0900

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomPaginationActionsTable from './component/Dashboard'
+
+function App() {
+  return (
+    <div className="App">
+      <CustomPaginationActionsTable/>
+    </div>
+  );
+}
+
+export default App;
