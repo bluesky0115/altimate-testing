93f5579f5e02b71f1ef877033d76dc298f93383a
holinight <asmakech115@gmail.com>
2023-01-17 01:38:27 +0900

    get all data using fetch

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -16,6 +16,12 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import LastPageIcon from '@mui/icons-material/LastPage';
 import { TableHead } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function TablePaginationActions(props) {
   const theme = useTheme();
@@ -82,6 +88,7 @@ export default function CustomPaginationActionsTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 	const [rows, SetRows ] = React.useState([]);
+	const [open, setOpen] = React.useState(false);
 	
 	React.useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/posts')
@@ -102,63 +109,102 @@ export default function CustomPaginationActionsTable() {
     setPage(0);
   };
 
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-        <TableHead>
-					<TableRow>
-            <TableCell align='center'>No</TableCell>
-            <TableCell align='center'>Title</TableCell>
-            <TableCell align='center'>User ID</TableCell>
-            <TableCell align='center'>Completed Status</TableCell>
-					</TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row, ind) => (
-            <TableRow key={ind}>
-            	<TableCell>{page * rowsPerPage + ind + 1}</TableCell>
-              <TableCell width={500}>
-                {row.title}
-              </TableCell>
-              <TableCell>
-                {row.userId}
-              </TableCell>
-              <TableCell>
-                {row.completed ? 'true' : 'false'}
-              </TableCell>
-            </TableRow>
-          ))}
+	const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+		<>
+			<div>
+				<Button variant="outlined" onClick={handleClickOpen}>
+					Add
+				</Button>
+				<Dialog
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="alert-dialog-title"
+					aria-describedby="alert-dialog-description"
+				>
+					<DialogTitle id="alert-dialog-title">
+						{"Use Google's location service?"}
+					</DialogTitle>
+					<DialogContent>
+						<DialogContentText id="alert-dialog-description">
+							Let Google help apps determine location. This means sending anonymous
+							location data to Google, even when no apps are running.
+						</DialogContentText>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Disagree</Button>
+						<Button onClick={handleClose} autoFocus>
+							Agree
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</div>
+			<div>
+				<TableContainer component={Paper}>
+					<Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+						<TableHead>
+							<TableRow>
+								<TableCell align="center">No</TableCell>
+								<TableCell align="center">Title</TableCell>
+								<TableCell align="center">User ID</TableCell>
+								<TableCell align="center">Completed Status</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{(rowsPerPage > 0
+								? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+								: rows
+							).map((row, ind) => (
+								<TableRow key={ind}>
+									<TableCell align="center">{page * rowsPerPage + ind + 1}</TableCell>
+									<TableCell component="th" scope="row" align="center">
+										{row.title}
+									</TableCell>
+									<TableCell align="center">
+										{row.userId}
+									</TableCell>
+									<TableCell align="center">
+										{row.completed ? 'true' : 'false'}
+									</TableCell>
+								</TableRow>
+							))}
+
+							{emptyRows > 0 && (
+								<TableRow style={{ height: 53 * emptyRows }}>
+									<TableCell colSpan={6} />
+								</TableRow>
+							)}
+						</TableBody>
+						<TableFooter>
+							<TableRow>
+								<TablePagination
+									rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+									colSpan={3}
+									count={rows.length}
+									rowsPerPage={rowsPerPage}
+									page={page}
+									SelectProps={{
+										inputProps: {
+											'aria-label': 'rows per page',
+										},
+										native: true,
+									}}
+									onPageChange={handleChangePage}
+									onRowsPerPageChange={handleChangeRowsPerPage}
+									ActionsComponent={TablePaginationActions}
+								/>
+							</TableRow>
+						</TableFooter>
+					</Table>
+				</TableContainer>
+			</div>
+		</>
   );
 }
